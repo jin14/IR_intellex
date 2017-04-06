351f1c64c2b329759022a6736c7b128096944b5a
JIN WEI TAN <jinweitan@204-189.priv23.nus.edu.sg>
2017-04-06 19:57:43 +0800

    dictionary,postings,metadata files done

--- Index.py
+++ Index.py
@@ -32,7 +32,47 @@ def stemmed_tags(tags):
     return result
 
 
-def make_dictionary(directory,dictionary_file,postings_file):
+def storein_metadata(metadata_file,metadata,index,key):
+    
+    index['metadata'][key] = {}
+    
+    for term in metadata:
+        start = metadata_file.tell()
+        postings = ','.join(map(str,metadata[term]))
+        metadata_file.write(postings)
+        index['metadata'][key][term] ={'start': start, 'len': len(postings)}
+
+    return index
+
+
+def store_content(posting_file,content,index,key,idfs):
+    
+    for term in content:
+        index[key][term] = {'idf': idfs[term]}
+        for docid in content[term]:
+            start = posting_file.tell()
+            postings = ','.join(map(str,content[term][docid]['index']))
+            posting_file.write(postings)
+            index[key][term][docid] = {'start':start, 'len':len(postings), 'tf': content[term][docid]['tf']}
+    
+    return index
+
+
+def store_title(posting_file,title,index,key):
+    
+    for term in title:
+        index[key][term] = {}
+        for docid in title[term]:  
+            start = posting_file.tell()
+            postings = ','.join(map(str,title[term][docid]))
+            posting_file.write(postings)
+            index[key][term][docid] = {'start': start, 'len':len(postings)}
+            
+    
+    return index
+
+
+def make_dictionary(directory,dictionary_file,postings_file,metadata_file):
     
     filenames = os.listdir(directory)
     if directory[-1]!='/':
@@ -46,6 +86,8 @@ def make_dictionary(directory,dictionary_file,postings_file):
     A_jurisdiction = {}
     A_court = {}
     tfs = {}
+    #idf
+    idfs = {}
     total_df = len(filenames)
     #print(total_df)
     
@@ -88,25 +130,25 @@ def make_dictionary(directory,dictionary_file,postings_file):
             contentsize = len(content)
             for index,term in enumerate(content):
                 if term not in A_content: #new term, new docid
-                    A_content[term] = {docid:[index]}
+                    A_content[term] = {docid:{'index':[index],'tf':None}}
 
                 else: # term exists
                     if docid not in A_content[term]: # if docid does not exists
-                        A_content[term][docid] = [index]
+                        A_content[term][docid] = {'index':[index], 'tf':None}
 
                     else: #docid exists
-                        A_content[term][docid].append(index)
+                        A_content[term][docid]['index'].append(index)
             
             length = []
             for term in A_content:
                 if docid in A_content[term]:
                     if term not in tfs:
                         tfs[term] = {}
-                        tfreq = tf(len(A_content[term][docid])) #log term frequency
+                        tfreq = tf(len(A_content[term][docid]['index'])) #log term frequency
                         tfs[term][docid] = tfreq
                         length.append(tfreq)
                     else:
-                        tfreq = tf(len(A_content[term][docid])) #log term frequency
+                        tfreq = tf(len(A_content[term][docid]['index'])) #log term frequency
                         tfs[term][docid] = tfreq
                         length.append(tfreq)
 
@@ -114,7 +156,10 @@ def make_dictionary(directory,dictionary_file,postings_file):
                 if docid in tfs[term]:
                     tfs[term][docid] = tfs[term][docid]/L2norm(length) #normalisation
 
-                    
+            
+            for term in A_content:
+                if docid in A_content[term]:
+                    A_content[term][docid]['tf'] = tfs[term][docid]
         
         #title (positional indexes)
         if 'title' in d.keys():
@@ -154,7 +199,6 @@ def make_dictionary(directory,dictionary_file,postings_file):
     
     for term in A_content:
         df = len(A_content[term])
-        print(df)
         idfs[term] = idf(df,total_df)
     
     
@@ -162,56 +206,36 @@ def make_dictionary(directory,dictionary_file,postings_file):
     
     dictionary = open(dictionary_file,'w')
     postings = open(postings_file,'w')
-    index = {}
-    result = {'tags': A_tags, 'date_posted': A_dateposted, 'content': A_content, 'title': A_title, 'jur': A_jurisdiction, 'court': A_court}
-    for key in result:
-
-        if key == 'content' or key == 'title':
-            for term,value in result[key].items():
-                for docid,position in result[key][term].items():
-                    if term not in index:
-                        start = postings.tell()
-                        posting = ','.join(map(str,result[key][term][docid]))
-                        posting = 's'+ str(docid) + ',' + posting
-                        postings.write(posting)
-                        index[term] = {str(key): start, str(key)+'len': len(posting)}
-                    else:
-                        start = postings.tell()
-                        posting = ','.join(map(str,result[key][term][docid]))
-                        posting = str(docid) + ',' + posting    
-                        postings.write(posting)
-                        index[term][str(key)] = start
-                        index[term][str(key)+'len'] = len(posting)
-                        
-        else:
-            for term,docids in result[key].items():
-                if term not in index:
-                    start = postings.tell()
-                    posting = ','.join(map(str,docids))
-                    postings.write(posting)
-                    index[term] = {str(key):start, str(key)+'len': len(posting)}
-                else:
-                    start = postings.tell()
-                    posting = ','.join(map(str,docids))
-                    postings.write(posting)
-                    index[term][str(key)] = start
-                    index[term][str(key)+'len'] = len(posting)
-                    
+    metadata = open(metadata_file,'w')
+    
+    index = {'metadata':{}, 'content': {}, 'title': {} }
+    
+    #add in meta data
+    index = storein_metadata(metadata,A_tags,index,'tags')
+    index = storein_metadata(metadata,A_dateposted,index,'date_posted')
+    index = storein_metadata(metadata,A_jurisdiction,index,'jur')
+    index = storein_metadata(metadata,A_court,index,'court')
+    
+    #add in content
+    index = store_content(postings,A_content,index,'content',idfs)
+    
+    #add in title
+    index = store_title(postings,A_title,index,'title')
     
     
     json.dump(index,dictionary)
     dictionary.close()
     postings.close()
-
-    return idfs
+    metadata.close()
+    
 
 
 def usage():
-    print ("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+    print ("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file -m metadata_file")
 
-directory_of_documents = dictionary_file = postings_file = None
+directory_of_documents = dictionary_file = postings_file = metadata_file = None
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:m:')
 except (getopt.GetoptError, err):
     usage()
     sys.exit(2)
@@ -222,8 +246,10 @@ for o, a in opts:
         dictionary_file = a
     elif o == '-p':
         postings_file = a
+    elif o == '-m':
+        metadata_file = a
     else:
         assert False, "unhandled option"
-if directory_of_documents == None or dictionary_file == None or postings_file == None:
+if directory_of_documents == None or dictionary_file == None or postings_file == None or metadata_file == None:
     usage()
     sys.exit(2)
